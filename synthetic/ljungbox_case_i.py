5223104aac728ce6bdd4d99253ef2b994f22a2eb
saerdna-se <34302335+saerdna-se@users.noreply.github.com>
2017-12-08 09:27:38 +0100

    Update ljungbox_case_i.py

--- synthetic/ljungbox_case_i.py
+++ synthetic/ljungbox_case_i.py
@@ -2,7 +2,7 @@
 """
 Source code for the Ljung-Box part of the second synthetic example (Repeated evaluation of Case , Figure 3) in
 "Is My Model Flexible Enough? Information-Theoretic Model Check",
-Andreas Svensson, Dave Zachariah, Thomas B. Schön arXiv:NNNN
+Andreas Svensson, Dave Zachariah, Thomas B. Schön arXiv:1712.02675
 
 Code by Andreas Svensson 2017
 """
